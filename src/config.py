0491f91f2dcc32c3e6a9a0e9d9c3708ae0d4ca7f
advanced12iq <kkrasavecc@gmail.com>
2025-02-03 12:17:41 +0300

    Edited model.pth

--- src/config.py
+++ src/config.py
@@ -30,6 +30,6 @@ LEARNING_RATE = 0.001
 NUM_EPOCHS = 30
 MARGIN = 1.0
 
-MODEL_SAVE_PATH = os.path.join(BASE_DIR, 'models', 'siamese_model.pth')
+MODEL_SAVE_PATH = os.path.join(BASE_DIR, 'models', 'siamese_model.py')
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
